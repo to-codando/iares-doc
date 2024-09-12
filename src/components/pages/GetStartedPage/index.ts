2ab2d02fc70cef5ff322f15be5bf6336163c7bd3
Rodrigo Rocha <rocha.webdev@gmail.com>
2024-09-12 15:32:05 -0300

    feat: get started page english translation

--- src/components/pages/GetStartedPage/index.ts
+++ src/components/pages/GetStartedPage/index.ts
@@ -6,30 +6,28 @@ import { css, tsx, mdx } from "iares";
 const ContentApp = () => mdx`
 # Getting Started
 ---
+IARES is an open-source project created to help build all types of commercial applications with web technologies. However, unlike many frameworks and libraries, IARES doesn't turn into the next problem in your tech stack.
 
-IARES é open source e foi projetado para ajudar a construir e entregar sistemas rapidamente e ao contrário da maioria dos frameworks, não se tornará o próximo impecilho na sua pilha de tecnologias.
-
-A documentação de IARES é pequena, clara e eficiente cobrindo tudo o que é necessário saber sobre a ferramenta.
+The IARES documentation is intentionally brief but clear and understandable. Therefore, in this documentation, you can learn all about developing with IARES.
 
 ## Installation
 
-Você pode querer desenvolver aplicativos para web, mobile ou desktop. Por isso, preparei um template para cada tipo de projeto.
+Maybe you want to develop applications for web, mobile, or desktop. Hence, I prepared an appropriate template for each project type.
 
-### Templates
+### Template types
 
-Cada template trata dedicadamente as necessidades arquiteturais básicas, de cada tipo de sistema e define a estrutura inicial do projeto e suas dependências genéricas.
+Each template works with a specific set of tools to meet the necessities of each project type.
+
+- **esbuild**: Web development (SPA).
+
+- **Bun**: Web development (SSR or SSG).
+
+- **Capacitor**: Mobile development (IOS e Android).
+
+- **Tauri**: Desktop development (Windows, Mac OS or Linux).
 
 
-- **esbuild** - Desenvolvimento web com renderização SPA.
-
-- **Bun** - Desenvolvimento web com renderização SSR ou SSG.
-
-- **Capacitor** - Desenvolvimento mobile (IOS e Android).
-
-- **Tauri** - Desenvolvimento desktop (Windows, Mac OS e Linux).
-
-
-Apenas copie uma das instruções abaixo e cole no terminal do seu sistema operacional para criar um novo projeto com IARES.
+Just copy and paste any of the following terminal commands into your terminal, and you will get a project bootstrap to start application development.
 
 #### Web ( SPA )
 
@@ -64,21 +62,19 @@ npx degit to-codando/iares-template-desktop new-desktop-app-name
 
 ## Launch the application
 
-Para inicializar a aplicação, primeiro é necessário instalar todas as dependências. Faça isso, executando o comando abaixo no terminal, após acessar o diretório do projeto.
+To execute the project, you first need to install all application dependencies. Therefore, use the following command:
 
 ~~~raw
 pnpm i
 ~~~
 
-Com todas as dependências instaladas, você pode executar a aplicação através do comando:
-
+With all dependencies installed, you can run the application using the following command:
 
 ~~~raw
 pnpm dev
 ~~~
 
-Se você executou os comandos corretamente, sua aplicação já está acessível no navegador através do endereço: *https://seu.ip:3000*
-
+If you performed all the steps correctly, the application will be available in your browser through the address: **https://your.ip:3000**
 `;
 
 const template = () => tsx`
