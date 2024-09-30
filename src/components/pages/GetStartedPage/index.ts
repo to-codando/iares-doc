1b3b04e862b0a0085f4b90aff11ad0cd151d61ba
Rodrigo Rocha <rocha.webdev@gmail.com>
2024-09-30 16:24:22 -0300

    feat: include environment config instrucions

--- src/components/pages/GetStartedPage/index.ts
+++ src/components/pages/GetStartedPage/index.ts
@@ -6,55 +6,50 @@ import { css, tsx, mdx } from "iares";
 const ContentApp = () => mdx`
 # Getting Started
 ---
-IARES is an open-source project created to help build all types of commercial applications with web technologies. However, unlike many frameworks and libraries, IARES doesn't turn into the next problem in your tech stack.
+IARES is an open-source project created to help build all types of commercial applications with web technologies. However, unlike many frameworks and libraries, IARES doesn't become the next problem in your tech stack.
 
-The IARES documentation is intentionally brief but clear and understandable. Therefore, in this documentation, you can learn all about developing with IARES.
+The IARES documentation is intentionally brief but clear and understandable. Therefore, in this documentation, you can learn everything about developing with IARES.
 
 ## Installation
 
-Maybe you want to develop applications for web, mobile, or desktop. Hence, I prepared an appropriate template for each project type.
+You may want to develop applications for web, mobile, or desktop. Hence, I have prepared an appropriate template for each project type.
 
 ### Template types
 
 Each template works with a specific set of tools to meet the necessities of each project type.
 
 - **esbuild**: Web development (SPA).
-
 - **Bun**: Web development (SSR or SSG).
-
-- **Capacitor**: Mobile development (IOS e Android).
-
-- **Tauri**: Desktop development (Windows, Mac OS or Linux).
-
+- **Capacitor**: Mobile development (iOS and Android).
+- **Tauri**: Desktop development (Windows, Mac OS, or Linux).
 
 Just copy and paste any of the following terminal commands into your terminal, and you will get a project bootstrap to start application development.
 
-#### Web ( SPA )
+#### Web (SPA)
 
 ~~~raw
 npx degit to-codando/iares-template-spa new-spa-app-name
 ~~~
 
-#### Web ( SSR )
+#### Web (SSR)
 
 ~~~raw
 npx degit to-codando/iares-template-ssr new-ssr-app-name
 ~~~
 
-
-#### Web ( SSG )
+#### Web (SSG)
 
 ~~~raw
 npx degit to-codando/iares-template-ssg new-ssg-app-name
 ~~~
 
-#### Mobile ( Android e IOS )
+#### Mobile (Android and iOS)
 
 ~~~raw
 npx degit to-codando/iares-template-mobile new-mobile-app-name
 ~~~
 
-#### Desktop ( Windows, Mac OS, Linux )
+#### Desktop (Windows, Mac OS, Linux)
 
 ~~~raw
 npx degit to-codando/iares-template-desktop new-desktop-app-name
@@ -62,10 +57,43 @@ npx degit to-codando/iares-template-desktop new-desktop-app-name
 
 ## Launch the application
 
-To execute the project, you first need to install all application dependencies. Therefore, use the following command:
+To run the project, you first need to install all application dependencies. Therefore, use the following command:
 
 ~~~raw
 pnpm i
+~~~
+
+The IARES templates are already prepared to serve the application locally using HTTPS through your computer's IP address. However, you can change this behavior by adjusting the information in the **.env** file, which you must create manually at the root of your project with instructions similar to those below:
+
+~~~raw
+# BUILD MODE
+# VALUES: production or development
+NODE_ENV=development
+
+PROTOCOL=https
+
+# NETWORK PORT
+PORT=3000
+
+# USE IN PRODUCTION BUILD
+PROD_HOST=localhost
+
+# USE IN DEVELOPMENT
+# THIS IS THE IP ADDRESS OF THE DEVELOPMENT MACHINE
+DEV_HOST=192.168.18.22
+
+# HTTPS IN DEV MODE
+TLS_KEY=server.key
+TLS_CERT=server.crt
+~~~
+
+You also need to create the required TLS certificate files to serve the application through a self-signed secure certificate.
+
+If you want to remove the HTTPS configuration and serve the application on localhost using only HTTP protocol, simply set the **PROTOCOL** variable in the **.env** file to **http** and remove the following configuration from the **esbuild.config.js** file:
+
+~~~js
+keyfile: KEYPEM,
+certfile: CERTPEM,
 ~~~
 
 With all dependencies installed, you can run the application using the following command:
@@ -74,7 +102,8 @@ With all dependencies installed, you can run the application using the following
 pnpm dev
 ~~~
 
-If you performed all the steps correctly, the application will be available in your browser through the address: **https://your.ip:3000**
+If you followed all the steps correctly, the application will be available in your browser at the address: **https://your.ip:3000**
+
 `;
 
 const template = () => tsx`
